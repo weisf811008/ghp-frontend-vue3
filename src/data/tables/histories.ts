9d0b87d5b46169bbef237156ba2a0f2e3275ad08
Claire <weisf811008@gmail.com>
2026-07-14 11:31:16 +0800

    feat: add '操作內容' header to history table

--- src/data/tables/histories.ts
+++ src/data/tables/histories.ts
@@ -2,5 +2,6 @@ export const historyHeaders = [
   { title: '項次', key: 'index', width: '80px' },
   { title: '帳號', key: 'username' },
   { title: '使用者', key: 'name' },
+  { title: '操作內容', key: 'message' },
   { title: '修改日期/時間', key: 'timestamp' },
 ]
